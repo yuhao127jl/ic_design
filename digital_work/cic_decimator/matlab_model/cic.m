aa0f0ad5ecb3928c60f3e1b5224bb0ffa65cd17b
yuhao127 <yushaodi127@foxmail.com>
2019-07-22 08:48:49 +0800

    1. modified model output width 	modified:   cic.m

--- digital_work/cic_decimator/matlab_model/cic.m
+++ digital_work/cic_decimator/matlab_model/cic.m
@@ -9,7 +9,7 @@ R = 32; % decimator rate is 32
 N = 3;  % CIC level
 
 xn = 100 .* ones(1,300);
-q = quantizer([8 0], 'fixed');
+q = quantizer([15 0], 'fixed');
 
 ym = cicdecimate(D, N, R, xn, q);
 
