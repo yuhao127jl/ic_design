cd45266b86ba88239780a508fdb564bcc084bd32
yuhao127 <yushaodi127@foxmail.com>
2019-08-10 17:11:03 +0800

    1. Add UVM env - driver, sequence, sequencer 	new file:   driver.sv 	new file:   driver_rst_sequence.sv 	new file:   host_io.sv 	new file:   router_io.sv 	new file:   top.sv

---
+++ uvm_work/work/router_ral_test/sv/driver.sv
@@ -0,0 +1,100 @@
+
+class driver extends uvm_driver #(packet);
+	virtual router_io router_vif;
+	int		port_id = -1;
+	logic [7:0] datnum;
+	
+	`uvm_component_utils_begin(driver)
+		`uvm_field_int(port_id, UVM_DEFAULT | UVM_DEC)
+	`uvm_component_utils_end
+
+	function new(string name, uvm_component parent);
+		super.new(name, parent);
+	endfunction
+
+	//-----------------------------------------//
+	// build phase
+	//-----------------------------------------//
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+
+		uvm_config_db#(int)::get(this, "", "port_id", port_id);
+		if(!(port_id inside {-1, [0:15]}))
+		begin
+			`uvm_fatal("CFG_ERROR", $sformatf("port_id must be {-1, [0:15]}, not %0d!", port_id));
+		end
+		uvm_config_db#(virtual router_io)::get(this, "", "m_vif", router_vif);
+		if(router_vif==null)
+		begin
+			`uvm_fatal("CFG_ERROR", "Interface for Driver not set");
+		end
+	endfunction
+
+	//-----------------------------------------//
+	// start_of_simulation_phase
+	//-----------------------------------------//
+	function void start_of_simulation_phase(uvm_phase phase);
+		super.start_of_simulation_phase(phase);
+		`uvm_info("DRV_CFG", $sformatf("port_id is : %0d", port_id), UVM_MEDIUM);
+	endfunction
+	
+	//-----------------------------------------//
+	// run_phase
+	//-----------------------------------------//
+	virtual task run_phase(uvm_phase phase);
+		forever 
+		begin
+			seq_item_port.get_next_item(req);
+			
+			if(port_id inside {-1, req.sa})
+			begin
+				send(req);
+				`uvm_info("DRV_RUN", {"\n", req.sprint()}, UVM_MEDIUM);
+			end
+
+			seq_item_port.item_done();
+		end
+	endtask
+
+	//-----------------------------------------//
+	// task send()
+	//-----------------------------------------//
+	virtual task send(packet tr);
+		send_address(tr);
+		send_pad(tr);
+		send_payload(tr);
+	endtask
+	
+	virtual task send_address(packet tr);
+		router_vif.drvClk.frame_n[tr.sa] <= 1'b0;
+		for(int i=0; i<4; i++)
+		begin
+			router_vif.drvClk.din[tr.sa] <= tr.da[i];
+			@(router_vif.drvClk);
+		end
+	endtask
+
+	virtual task send_pad(packet tr);
+		router_vif.drvClk.din[tr.sa] <= 1'b1;
+		router_vif.drvClk.valid_n[tr.sa] <= 1'b1;
+		repeat(5) @(router_vif.drvClk);
+	endtask
+
+	virtual task send_payload(packet tr);
+		while(!router_vif.drvClk.busy_n[tr.sa]) @(router_vif.drvClk);
+		foreach(tr.payload[index])
+		begin
+			datnum = tr.payload[index];
+			for(int i=0; i<$size(tr.payload,2); i++)
+			begin
+				router_vif.drvClk.din[tr.sa] <= datnum[i];
+				router_vif.drvClk.valid_n[tr.sa] <= 1'b0;
+				router_vif.drvClk.frame_n[tr.sa] <= ((tr.payload.size()-1) == index) && (i==7);
+				@(router_vif.drvClk);
+			end
+		end
+		router_vif.valid_n[tr.sa] <= 1'b1;
+	endtask
+
+endclass
+
---
+++ uvm_work/work/router_ral_test/sv/driver_rst_sequence.sv
@@ -0,0 +1,67 @@
+
+class driver_rst_sequence extends uvm_sequence #(packet);
+	virtual router_io router_vif;
+	int		port_id = -1;
+
+	`uvm_component_utils_begin(driver_rst_sequence)
+		`uvm_field_int(port_id, UVM_DEFAULT | UVM_DEC)
+	`uvm_component_utils_end
+
+	function new(string name = "driver_rst_sequence");
+		super.new(name);
+	endfunction
+
+	//-----------------------------------------//
+	// pre_start task
+	//-----------------------------------------//
+	virtual task pre_start();
+		if((get_parent_sequence()==null) && (starting_phase!=null))
+		begin
+			starting_phase.drop_objection(this);
+		end
+		uvm_config_db#(int)::get(get_sequencer(), "", "port_id", port_id);
+
+		if(!(port_id inside {-1, [0:15]}))
+		begin
+			`uvm_fatal("CFG_ERROR", $sformatf("port_id must be {-1, [0:15]}, not %0d!", port_id));
+		end
+		`uvm_info("DRV_RST_SEQ", $sformatf("Using port_id = %0d", port_id), UVM_MEDIUM);
+
+		uvm_config_db#(virtual router_io)::get(get_sequencer(), "", "m_vif", router_vif);
+		if(router_vif==null)
+		begin
+			`uvm_fatal("CFG_ERROR", "Interface for Driver Rst Sequence not set");
+		end
+	endtask
+
+	//-----------------------------------------//
+	// post_start task
+	//-----------------------------------------//
+	virtual task post_start();
+		if((get_parent_sequence()==null) && (starting_phase!=null))
+		begin
+			starting_phase.drop_objection(this);
+		end
+	endtask
+
+	//-----------------------------------------//
+	// body task
+	//-----------------------------------------//
+	virtual task body();
+		if(port_id == -1)
+		begin
+			router_vif.frame_n = '1;
+			router_vif.valid_n = '1;
+			router_vif.din 	= '0;
+		end
+		else
+		begin
+			router_vif.frame_n[port_id] = '1;
+			router_vif.valid_n[port_id] = '1;
+			router_vif.din[port_id] 	= '0;
+		end
+	endtask
+
+
+endclass
+
