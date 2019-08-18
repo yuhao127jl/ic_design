b6ee6a2b2c0941b1d5c62a23f8a07961eacb92db
yuhao127 <yushaodi127@foxmail.com>
2019-08-18 10:48:29 +0800

    1. Add ral_host_regmodel & virtual_reset_sequence.  Changes to be committed: 	modified:   host_ral_agent/ral_host_regmodel.sv 	new file:   router_env.sv 	new file:   virtual_reset_sequence.sv

--- uvm_work/work/router_ral_test/sv/host_ral_agent/ral_host_regmodel.sv
+++ uvm_work/work/router_ral_test/sv/host_ral_agent/ral_host_regmodel.sv
@@ -84,9 +84,87 @@ endclass
 //------------------------------------------------------------------//
 class ral_mem_RAM extends uvm_mem;
 
+	function new(string name = "RAM");
+		super.new(name, `UVM_REG_ADDR_WIDTH'h1000, 16, "RW", build_coverage(UVM_NO_COVERAGE));
+	endfunction
+
+	//-----------------------------------------//
+	// build
+	//-----------------------------------------//
+	virtual function void build();
+    endfunction
+
+    `uvm_object_utils(ral_mem_RAM)
 
 
 endclass
 
 
+//------------------------------------------------------------------//
+//
+// ral_block_host_regmodel
+//
+//------------------------------------------------------------------//
+class ral_block_host_regmodel extends uvm_reg_block;
+    rand ral_reg_HOST_ID    HOST_ID;
+    rand ral_reg_LOCK       LOCK;
+    rand ral_reg_R_ARRAY    R_ARRAY[256];
+    rand ral_mem_RAM        RAM;
+    uvm_reg_field           HOST_ID_REV_ID;
+    uvm_reg_field           REV_ID;
+    uvm_reg_field           HOST_ID_CHIP_ID;
+    uvm_reg_field           CHIP_ID;
+    rand uvm_reg_field      LOCK_LOCK;
+    rand uvm_reg_field      R_ARRAY_H_REG[256];
+    rand uvm_reg_field      H_REG[256];
+
+
+	function new(string name = "host_regmodel");
+		super.new(name, build_coverage(UVM_NO_COVERAGE));
+	endfunction
+
+	//-----------------------------------------//
+	// build
+	//-----------------------------------------//
+	virtual function void build();
+        this.default_map = create_map("", 0, 2, UVM_LITTLE_ENDIAN, 0);
+        this.HOST_ID = ral_reg_HOST_ID::type_id::create("HOST_ID", , get_full_name());
+        this.HOST_ID.configure(this, null, "");
+        this.HOST_ID.build();
+        this.HOST_ID.add_hdl_path('{'{"host_id", -1, -1}});
+        this.default_map.add_reg(this.HOST_ID, `UVM_REG_ADDR_WIDTH'h0, "RW", 0);
+
+        this.HOST_ID_REV_ID = this.HOST_ID.REV_ID;
+        this.REV_ID = this.HOST_ID.REV_ID;
+        this.HOST_ID_CHIP_ID = this.HOST_ID.CHIP_ID;
+        this.CHIP_ID = this.HOST_ID.CHIP_ID;
+
+        this.LOCK = ral_reg_LOCK::type_id::create("LOCK", , get_full_name());
+        this.LOCK.configure(this, null, "");
+        this.HOST_ID.build();
+        this.LOCK.add_hdl_path('{'{"lock", -1, -1}});
+        this.default_map.add_reg(this.LOCK, `UVM_REG_ADDR_WIDTH'h100, "RW", 0);
+
+        this.LOCK_LOCK = this.LOCK.LOCK;
+        foreach(this.R_ARRAY[i]) begin
+            int J =i;
+            this.R_ARRAY[J] = ral_reg_R_ARRAY::type_id::create($psprintf("R_ARRAY[%0d]", J), , get_full_name());
+            this.R_ARRAY[J].configure(this, null, "");
+            this.R_ARRAY[J].build();
+            this.R_ARRAY[J].add_hdl_path('{'{$psprintf("host_reg[%0d]",J), -1, -1}});
+            this.default_map.add_reg(this.R_ARRAY[J], `UVM_REG_ADDR_WIDTH'h1000+J*`UVM_REG_ADDR_WIDTH'h1, "RW", 0);
+            this.R_ARRAY_H_REG[J] = this.R_ARRAY[J].H_REG;
+            this.H_REG[J] = this.R_ARRAY[J].H_REG;
+        end
+
+        this.RAM = ral_mem_RAM::type_id::create("RAM", , get_full_name());
+        this.RAM.configure(this, "RAM");
+        this.RAM.build();
+        this.default_map.add_reg(this.RAM, `UVM_REG_ADDR_WIDTH'h4000, "RW", 0);
+    endfunction
+
+    `uvm_object_utils(ral_block_host_regmodel)
+
+
+endclass
 
---
+++ uvm_work/work/router_ral_test/sv/virtual_reset_sequence.sv
@@ -0,0 +1,76 @@
+
+//----------------------------------------------------------//
+//
+// virtual_reset_sequencer
+//
+//----------------------------------------------------------//
+class virtual_reset_sequencer extends uvm_sequencer;
+    `uvm_component_utils(virtual_reset_sequence)
+    packet_sequencer pkt_seqr[$];
+    reset_sequencer r_seqr;
+    host_sequencer h_seqr;
+
+	function new(string name, uvm_component parent);
+		super.new(name, parent);
+	endfunction
+    
+endclass
+
+
+//----------------------------------------------------------//
+//
+// virtual_reset_sequence
+//
+//----------------------------------------------------------//
+class virtual_reset_sequence extends uvm_sequence;
+    `uvm_object_utils(virtual_reset_sequence)
+    `uvm_declare_p_sequencer(virtual_reset_sequencer)
+
+    reset_sequence r_seq;
+    driver_rst_sequence d_seq;
+    host_reset_sequence h_seq;
+    uvm_event reset_event = uvm_event_pool::get_global("reset");
+
+	function new(string name = "virtual_reset_sequence");
+		super.new(name);
+	endfunction
+
+	//-----------------------------------------//
+	// body task
+	//-----------------------------------------//
+	virtual task body();
+        fork
+            `uvm_do_on(r_req, p_sequencer.r_seqr);
+            foreach(p_sequencer.pkt_seqr[i]) begin
+                int j = i;
+                fork 
+                    begin
+                        reset_event.wait_on();
+                        `uvm_do_on(d_req, p_sequencer.pkt_seqr[j]);
+                    end
+                join_none
+            end
+
+            begin
+                reset_event.wait_on();
+                `uvm_do_on(h_req, p_sequencer.h_seqr);
+            end
+        join
+    endtask
+
+	//-----------------------------------------//
+	// post_start
+	//-----------------------------------------//
+    virtual task post_start();
+        if((get_parent_sequence() == null) && (starting_phase != null))
+        begin
+            starting_phase.drop_objection(this);
+        end
+    endtask
+
+
+
+
+endclass
+
+
